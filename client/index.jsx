1c68c884e81598675fa9ec566abf7bb5c4649452
Doug Riblet <smashzen85@gmail.com>
2021-04-18 22:53:32 -0500

    set up ApolloClient and ApolloProvider

--- client/index.jsx
+++ client/index.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client/react';
 import App from './components/App';
 
+const client = new ApolloClient({
+  uri: 'https://localhost:8246',
+  cache: new InMemoryCache()
+});
+
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <ApolloProvider client={client}>
+    <Router>
+      <App />
+    </Router>
+  </ApolloProvider>,
   document.getElementById('root'),
 );

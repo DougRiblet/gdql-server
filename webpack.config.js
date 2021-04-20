9ea59259f0255b2ba02ab866e658b00876008b5a
Doug Riblet <smashzen85@gmail.com>
2021-04-20 13:12:25 -0500

    edit webpack config, add loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,27 @@
-import path from 'path';
+const path = require("path");
 
 module.exports = {
-  entry: './client/index.jsx',
+  entry: { index: path.resolve(__dirname, "client", "index.js") },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, "client", "dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "client", "dist", "index.html")
+    })
+  ],
 };
